bcd6f97c75d0a3f9f1504e897a95188738716339
Miquel Massot <miquel.massot@uib.cat>
2013-01-08 22:20:13 +0100

    Comments on mouse_control

--- src/mouse_control.cpp
+++ src/mouse_control.cpp
@@ -7,11 +7,14 @@ MouseControl::MouseControl(){
 }
 
 void MouseControl::init(){
+  // Save the working display and check that it's ok
   dpy_ = XOpenDisplay((NULL));
   assert(dpy_);
 }
 
 void MouseControl::grabMouse(){
+  // Mouse grabbing allows the programmer to stop any mouse movement
+  // from the user. Needs ungrabbing to return the control to enduser!
   Screen *screen;
   if(getMousePosition()){
 	  screen = ScreenOfDisplay(dpy_, screen_num_);
@@ -22,16 +25,22 @@ void MouseControl::grabMouse(){
 }
 
 void MouseControl::ungrabMouse(){
+  // Undoes mouse grabbing.
   XUngrabPointer(dpy_, CurrentTime);
 }
 
 void MouseControl::setMousePosition(int x_in, int y_in){
+  // Warps mouse position to the desired location in the screen
   Window screen_root = RootWindow(dpy_, screen_num_);
   XWarpPointer(dpy_, None, screen_root, 0, 0, 0, 0, x_in, y_in);
+  // Flush is needed in order to refresh the screen and the mouse to
+  // the desired position!
   XFlush(dpy_);
 }
 
 int MouseControl::getMousePosition(){
+  // Saves actual mouse position in this.x and this.y
+  // and returns 1 if has found the mouse, 0 if it's lost...
   int x_out=0;
   int y_out=0;
   int dummy_int=0;
